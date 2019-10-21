7a244d84ff0be5b16340a0c4ec6d12c1239c2993
kamikeeen <unjo.kamiken@gmail.com>
2019-10-21 17:16:23 +0900

    #2 modify-deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, "https://github.com/kamikeeen/find-likes.git"
 ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp}.call
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, fetch(:linked_files, []).push("config/master.key")
+# set :linked_files, fetch(:linked_files, []).push("config/master.key")
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
@@ -24,6 +24,19 @@ namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
+end
+
+desc "upload master.key"
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!("config/master.key", "#{shared_path}/config/master.key")
+    end
+  end
+  before :starting, "deploy:upload"
+  after :finishing, "deploy:cleanup"
 end
 
 
