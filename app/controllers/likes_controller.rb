7830a729e35296134025e2d8aeac580908257008
kamikeeen <unjo.kamiken@gmail.com>
2019-10-23 14:00:23 +0900

    #5 create-like-system
    likeの作成機能

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -9,6 +9,17 @@ class LikesController < ApplicationController
   end
 
   def create
+    @like = Like.new(like_params)
 
+    if @like.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def like_params
+    params.require(:like).permit(:name, :ancestry)
   end
 end
