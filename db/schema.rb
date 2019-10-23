d742fa212f01c3db235d795d31fb48b881c7f650
kamikeeen <unjo.kamiken@gmail.com>
2019-10-23 15:56:20 +0900

    #5 create-genre
    active_hashによるgenre追加
    likeテーブルにgenreカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_23_050151) do
+ActiveRecord::Schema.define(version: 2019_10_23_064123) do
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
     t.string "name"
     t.string "ancestry"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "genre_id", null: false
   end
 
   create_table "user_likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
