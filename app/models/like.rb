1ea8de239d82266c4a16a7eb00c36ea90106c148
kamikeeen <unjo.kamiken@gmail.com>
2019-10-23 15:58:30 +0900

    #5 create-association_genre-like

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,7 @@
 class Like < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :genre
+
   has_many :user_likes
   has_many :users, through: :user_likes
 
