8306200adc5f5e02cd77ea67cf675084a171c9dc
kamikeeen <unjo.kamiken@gmail.com>
2019-10-25 15:33:13 +0900

    #5 create-validation-for-item
    itemモデルへのバリデーションの追加

--- app/models/like.rb
+++ app/models/like.rb
@@ -5,4 +5,8 @@ class Like < ApplicationRecord
   has_many :user_likes
   has_many :users, through: :user_likes
 
+  validates :name,  presence: true,
+                    uniqueness: {scope: :genre_id, message: "すでに登録されています"} 
+  validates :genre_id, presence: true
+
 end
