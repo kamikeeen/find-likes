0f99c74dc1632d3a03cbe60f7da863576d82b989
kamikeeen <unjo.kamiken@gmail.com>
2019-10-27 18:06:16 +0900

    #9 create-user_sort
    ユーザーのソート機能の実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -21,6 +21,20 @@ class LikesController < ApplicationController
   end
 
   def show
+    user_like_ids = current_user.likes.ids
+    same_likes = {}
+    @like.users.each do |follow_user|
+      same_likes_array = follow_user.likes.ids & user_like_ids
+      same_likes[follow_user.id] = same_likes_array.length
+    end
+    same_likes = same_likes.sort{|(k1, v1), (k2, v2)| v2 <=> v1 }
+    user_ids = []
+    @same_like_counts = []
+    same_likes.each do |user|
+      user_ids << user[0]
+      @same_like_counts << user[1]
+    end
+    @users = User.where(id: user_ids).order(['field(id, ?)', user_ids])
   end
 
   def add
