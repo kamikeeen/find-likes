86728ccea83a22427b39531426d10a7607e4c416
kamikeeen <unjo.kamiken@gmail.com>
2019-10-27 20:51:10 +0900

    #10 edit-like_new-to-js

--- app/javascript/packs/like_new.js
+++ app/javascript/packs/like_new.js
@@ -1,15 +1,4 @@
 $(function(){
-  var genre_html = `<div class="like-form__detail">
-                      <label for="like_genre_id">ジャンルを選択してください</label>
-                      <br>
-                      <select name="like[genre_id]" id="like_genre_id"><option value="">---</option>
-                        <option value="1">映画</option>
-                        <option value="2">アニメ</option>
-                        <option value="3">音楽</option>
-                        <option value="4">漫画</option>
-                        <option value="5">その他</option></select>
-                    </div>
-                    <input type="submit" name="commit" value="好きを作成!" class="create-btn" data-disable-with="好きを作成!">`
 
   $("#search__like__box").on("keyup", function(){
     let keyword = $(this).val() 
@@ -32,8 +21,17 @@ $(function(){
     }
   })
   $(document).on("click", ".result__create-btn", function(){
-    $(".like-form__detail").remove()
-    $(".create-btn").remove()
-    $(".result").append(genre_html)
+    $.ajax({
+      url: "/api/likes/select_genre",
+      type: "get",
+      dataType: "html"
+    })
+    .done(function(html){
+      $(".like-form__detail").remove()
+      $(".create-btn").remove()
+      $(".result").append(html)
+    })
+    .fail(function(){
+    })
   })
 })
