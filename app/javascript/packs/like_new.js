9373ec92d1473b628ff510a88c3125c3db2b3b74
kamikeeen <unjo.kamiken@gmail.com>
2019-10-27 19:45:43 +0900

    #10 edit-likes_new
    likeの新規作成時の仕様変更
    検索してなければ、フォームがでる

--- app/javascript/packs/like_new.js
+++ app/javascript/packs/like_new.js
@@ -1,6 +1,19 @@
 $(function(){
+  var genre_html = `<div class="like-form__detail">
+                      <label for="like_genre_id">ジャンルを選択してください</label>
+                      <br>
+                      <select name="like[genre_id]" id="like_genre_id"><option value="">---</option>
+                        <option value="1">映画</option>
+                        <option value="2">アニメ</option>
+                        <option value="3">音楽</option>
+                        <option value="4">漫画</option>
+                        <option value="5">その他</option></select>
+                    </div>
+                    <input type="submit" name="commit" value="好きを作成！" class="create-btn" data-disable-with="好きを作成！">`
+
   $("#search__like__box").on("keyup", function(){
     let keyword = $(this).val() 
+    $("#like_name").val(keyword)
     if (keyword !== ""){
       $.ajax({
         url: "/api/likes/search",
@@ -9,13 +22,18 @@ $(function(){
         dataType: "html"
       })
       .done(function(html){
-        $(".search-results").remove();
+        $(".result").remove();
         $(".search__like").after(html)
       })
       .fail(function(){
       })
     }else{
-      $(".search-results").remove()
+      $(".result").remove()
     }
+  })
+  $(document).on("click", ".result__create-btn", function(){
+    $(".like-form__detail").remove()
+    $(".create-btn").remove()
+    $(".result").append(genre_html)
   })
 })
