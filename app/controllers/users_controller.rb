a02b8ce3c041d65bf18e98228763b70c3483ec4a
kamikeeen <unjo.kamiken@gmail.com>
2019-10-27 18:29:26 +0900

    #9 edit-user_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    same_likes = @user.likes & current_user.likes
+    @count = same_likes.length
   end
 end
