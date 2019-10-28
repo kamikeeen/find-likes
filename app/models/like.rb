ed442309be5de3683237f4148c7c39f35185a24d
kamikeeen <unjo.kamiken@gmail.com>
2019-10-28 15:56:18 +0900

    #12 create-validation_like

--- app/models/like.rb
+++ app/models/like.rb
@@ -6,7 +6,11 @@ class Like < ApplicationRecord
   has_many :users, through: :user_likes
 
   validates :name,  presence: true,
-                    uniqueness: {scope: :genre_id, message: "すでに登録されています"} 
-  validates :genre_id, presence: true
-
+                    uniqueness: {scope: :genre_id, message: "すでに登録されています"},
+                    length: { maximum: 40 }
+                    
+  validates :genre_id,  presence: true,
+                        numericality:{only_integer: true,
+                                      greater_than_or_equal_to: 1,
+                                      less_than_or_equal_to: 8}
 end
