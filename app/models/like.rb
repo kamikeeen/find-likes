ca6edf711eece93aed30a2ffee695349225f9b46
kamikeeen <unjo.kamiken@gmail.com>
2019-10-29 16:20:28 +0900

    #14 create_ranking-to-toppage

--- app/models/like.rb
+++ app/models/like.rb
@@ -9,4 +9,14 @@ class Like < ApplicationRecord
                     uniqueness: {scope: :genre_id, message: "すでに登録されています"} 
   validates :genre_id, presence: true
 
+
+  def sorted_same_likes_users(current_user)
+    user_like_ids = current_user.likes.ids
+    same_likes = {}
+    self.users.each do |follow_user|
+      same_likes_array = follow_user.likes.ids & user_like_ids
+      same_likes[follow_user.id] = same_likes_array.length
+    end
+    same_likes = same_likes.sort{|(k1, v1), (k2, v2)| v2 <=> v1 }
+  end
 end
